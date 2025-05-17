24c93662367975811941ef7112b8b888851fe284
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2025-05-17 13:53:48 -0400

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -3,18 +3,30 @@ title Setup - PECG
 cd ..
 echo Installing dependencies, please wait...
 haxelib install hxcpp
-haxelib install flixel 5.6.1
-haxelib install flixel-addons 3.2.2
-haxelib install flixel-tools 1.5.1
-haxelib install hscript-iris 1.1.3
-haxelib install tjson 1.4.0
-haxelib install hxdiscord_rpc 1.2.4
-haxelib install hxvlc 2.0.1 --skip-dependencies
-haxelib install lime 8.1.2
-haxelib install openfl 9.3.3
-haxelib install flixel-ui 2.6.1
+haxelib install lime 8.0.0
+haxelib install openfl
+haxelib install flixel 4.11.0
+haxelib run lime setup flixel
+haxelib run lime setup         
+haxelib remove flixel-addons
+haxelib remove flixel-tools
+haxelib remove flixel-ui
+haxelib install flixel-tools
+haxelib install flixel-ui
+haxelib install flixel-addons 2.9.0
+haxelib install tjson
+haxelib install hxjsonast
+haxelib install hxCodec 2.5.1
+haxelib install linc_luajit
+haxelib install hscript
+haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
+haxelib git faxe https://github.com/uhrobots/faxe
+haxelib git polymod https://github.com/larsiusprime/polymod.git
+haxelib install hxcpp-debug-server
 haxelib install actuate 1.9.0 
-haxelib install hxCodec 2.5.1          
+haxelib install hxCodec 2.5.1
 haxelib install linc_luajit
 haxelib install hscript
 haxelib install newgrounds
