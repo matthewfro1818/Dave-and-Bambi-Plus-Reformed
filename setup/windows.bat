fd263bfddf23a6c98b5fa858640549b87e286e90
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2025-05-17 14:46:59 -0400

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -30,7 +30,7 @@ haxelib install linc_luajit
 haxelib install hscript
 haxelib install newgrounds
 haxelib install extension-webm
-haxelib install discord_rpc 1.0.0
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
